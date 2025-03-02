5e68bddb96fd29348d1ce2085ec76f91858e7207
Lic Valdez <johannyagustin@gmail.com>
2025-03-02 01:13:42 -0400

    Actualizar archivos app.js

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Crear un array para almacenar los nombres.
+let amigos = [];
+
+/**Implementar una función para agregar amigos.
+ * Agrega un amigo al array de amigos
+ * Valida que el campo nombre no este vacio
+ */
+function agregarAmigos(){
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if(nombreAmigo.trim() === ""){
+        alert("Por favor, inserte un nombre");
+    }else{
+        amigos.push(nombreAmigo);
+        document.querySelector("#amigo").value = "";
+        mostrarListaAmigo();
+    }
+}
+
+/**Actualizar el array de amigos.
+ */
+function mostrarListaAmigo(){
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let index = 0; index < amigos.length; index++){
+        const element = amigos[index];
+
+        let listaHTML = document.createElement("li");
+        listaHTML.textContent = element;
+        listaAmigos.appendChild(listaHTML);
+    }
+}
+
+/**Implementa una función para sortear los amigos.
+ * Validar que haya amigos disponibles.
+ * Generar un índice aleatorio.
+ * Obtener el nombre sorteado.
+ * Mostrar resultado.
+ */
+function sortearAmigo() {
+    let cantidadAmigos = amigos.length;
+    if(cantidadAmigos === 0) {
+        alert("Por favor, inserte un nombre antes de sortear");
+    }else {
+        let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+        let resultadoHTML = document.querySelector("#resultado");
+        resultadoHTML.innerHTML = amigos[indiceAmigo];
+    }
+}
+
+
